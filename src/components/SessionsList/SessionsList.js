331015f6587080519c05e0772b6c0b17bad216e5
cang <ho.kimcang@gmail.com>
2022-12-08 15:14:23 -0500

    restore state

---
+++ src/components/SessionsList/SessionsList.js
@@ -0,0 +1,14 @@
+//Styling
+import "./SessionsList.scss";
+
+import React from "react";
+
+const SessionsList = () => {
+  return (
+    <div className="sessions">
+      <h2 className="sessions__title">Active Sessions</h2>
+    </div>
+  );
+};
+
+export default SessionsList;
--- src/pages/SessionPage/SessionPage.js
+++ src/pages/SessionPage/SessionPage.js
@@ -1,37 +1,37 @@
-import React, { useState, useContext, useEffect } from "react";
-import "./SessionPage.scss";
+// import React, { useState, useContext, useEffect } from "react";
+// import "./SessionPage.scss";
 
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 
-import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
+// import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
-//Context
-import { SocketContext } from "../../SocketContext";
+// //Context
+// import { SocketContext } from "../../SocketContext";
 
-const SessionPage = ({ children }) => {
-  //Destructure global props from context
-  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
-    useContext(SocketContext);
-  const [idtoCall, setIdToCall] = useState("");
+// const SessionPage = ({ children }) => {
+//   //Destructure global props from context
+//   const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
+//     useContext(SocketContext);
+//   const [idtoCall, setIdToCall] = useState("");
 
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
-  const handleLeaveCall = () => {
-    leaveCall();
-    navigate("/");
-  }
+//   const handleLeaveCall = () => {
+//     leaveCall();
+//     navigate("/");
+//   }
 
-  return (
-    <section className="session">
-      <VideoPlayer />
+//   return (
+//     <section className="session">
+//       <VideoPlayer />
 
-      {/* <Link to="/"> */}
-        <div className="session__end-session" onClick={handleLeaveCall}>
-          <h2 className="home__call-text">End Call</h2>
-        </div>
-      {/* </Link> */}
-    </section>
-  );
-};
+//       {/* <Link to="/"> */}
+//         <div className="session__end-session" onClick={handleLeaveCall}>
+//           <h2 className="home__call-text">End Call</h2>
+//         </div>
+//       {/* </Link> */}
+//     </section>
+//   );
+// };
 
-export default SessionPage;
+// export default SessionPage;
