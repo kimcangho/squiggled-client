2bb33d50396c716f80f1d0b3c39d488dac2d256d
cang <ho.kimcang@gmail.com>
2023-01-07 17:48:29 -0500

    add join side for start page

--- src/pages/StartPage/StartPage.js
+++ src/pages/StartPage/StartPage.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
 import "./StartPage.scss";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 import squidLogo from "../../assets/images/logos/squiggled-logo.svg";
 
-const StartPage = ({ joinURL }) => {
+const StartPage = () => {
   const [flipped, setFlipped] = useState(false);
+  const [peerName, setPeerName] = useState("");
+  const [roomName, setRoomName] = useState("");
 
   const navigate = useNavigate();
 
@@ -16,25 +19,79 @@ const StartPage = ({ joinURL }) => {
   };
 
   const handleJoinSession = () => {
-    console.log("join");
-    setFlipped(true);
-    
+    setFlipped((value) => !value);
+  };
+
+  const handlePeerNameChange = (event) => {
+    setPeerName(event.target.value);
+  };
+
+  const handleRoomNameChange = (event) => {
+    setRoomName(event.target.value);
   };
 
   return (
     <main className="start">
       <div className="start__container">
-        <img className="start__logo" src={squidLogo} alt="Squiggled Logo" />
+        <CSSTransition in={flipped} timeout={300} classNames="start__flip">
+          <div className="start__card">
+            {/* Front Side */}
+            <div className="start__side start__side--front">
+              <img
+                className="start__logo"
+                src={squidLogo}
+                alt="Squiggled Logo"
+              />
+              <div className="start__panel">
+                <div className="start__button" onClick={handleSetupSession}>
+                  <h2 className="start__text">Start</h2>
+                </div>
 
-        <div className="start__panel">
-          <div className="start__button" onClick={handleSetupSession}>
-            <h2 className="start__text">Start</h2>
+                <div
+                  className="start__button start__button--join"
+                  onClick={handleJoinSession}
+                >
+                  <h2 className="start__text">Join</h2>
+                </div>
+              </div>
+            </div>
+            {/* Back Side */}
+            <div className="start__side start__side--back">
+              <form className="start__prompt">
+                <h5 className="start__text start__text--back">
+                  Join right in!
+                </h5>
+                <p className="start__body">Enter your name to join</p>
+                <input
+                  type="text"
+                  placeholder="Type your name"
+                  value={peerName}
+                  onChange={handlePeerNameChange}
+                  className="start__input"
+                ></input>
+                <p className="start__body">Enter room name to join</p>
+                <input
+                  type="text"
+                  placeholder="Input Room Name"
+                  value={roomName}
+                  onChange={handleRoomNameChange}
+                  className="start__input"
+                ></input>
+                <div className="start__panel">
+                  <div className="start__button" onClick={handleSetupSession}>
+                    <h2 className="start__text">Get Started</h2>
+                  </div>
+                  <div
+                    className="start__button start__button--back"
+                    onClick={handleJoinSession}
+                  >
+                    <h2 className="start__text">Go Back Out</h2>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-
-          <div className="start__button start__button--join">
-            <h2 className="start__text">Join</h2>
-          </div>
-        </div>
+        </CSSTransition>
       </div>
     </main>
   );
